960241be4d0b7b3a824a2e2107b9c0bd216ff241
sorikikikim <sorikim98@naver.com>
2022-04-15 14:28:34 +0900

    cpp07

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -1,7 +1,35 @@
+#include "whatever.hpp"
+
+template <typename T>
+void	swap(T& a, T& b)
+{
+	T tmp(a);
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+const T&	max(const T&a, const T&b)
+{
+	if (a > b)
+		return (a);
+	else
+		return (b);
+}
+
+template <typename T>
+const T&	min(const T&a, const T&b)
+{
+	if (a < b)
+		return (a);
+	else
+		return (b);
+}
+
 int main( void ) {
     int a = 2;
     int b = 3;
-    ::swap( a, b );
+    ::swap( a, b ); //global
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
     std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
